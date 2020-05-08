a879c6dd69363fc2409acf8eed36794b40a7a45d
qiguoqi <2280220550@qq.com>
2020-05-08 20:19:41 +0800

    change

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+// require('swiper/dist/css/swiper.css')
+import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 new Vue({
   router,
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HeaderSwiper",
+    data() {
+      return {
+        swiperOption: {
+          pagination: ".swiper-pagination",
+          loop: true
+        },
+        swiperList: [{
+          id: "0001",
+          imgUrl: "http://img1.qunarzz.com/sight/p0/1907/43/434697bdffd1475fa3.water.jpg_600x330_3a0f0c8d.jpg"
+        }, {
+          id: "0002",
+          imgUrl: "http://img1.qunarzz.com/sight/p0/1908/9c/9c0584bc05b14959a3.water.jpg_600x330_6bc563d9.jpg"
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 31.25%
+  background #eee
+  .swiper-img
+    width 100%
+    height 31.25vw
+</style>
