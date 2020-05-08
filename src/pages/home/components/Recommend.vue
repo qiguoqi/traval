0b8731b718d56ca041431cee3ae56281d7828655
qiguoqi <2280220550@qq.com>
2020-05-08 23:20:23 +0800

    Recommend weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <img :src="item.imgUrl" alt="" class="item-img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeRecommend",
+    data () {
+      return {
+        recommendList: [{
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/p0/1805/85/85c96c36663eeafea3.water.jpg_200x200_f6af6080.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, {
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/p0/1805/85/85c96c36663eeafea3.water.jpg_200x200_f6af6080.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, {
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/p0/1805/85/85c96c36663eeafea3.water.jpg_200x200_f6af6080.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, {
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/p0/1805/85/85c96c36663eeafea3.water.jpg_200x200_f6af6080.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl'
+.recommend-title
+  margin-top .2rem
+  background #eeeeee
+  line-height .8rem
+  text-indent .2rem
+.item
+  overflow hidden
+  display flex
+  height 1.9rem
+  .item-img
+    height 1.7rem
+    width 1.7rem
+    padding .1rem
+  .item-info
+    flex 1
+    min-width 0
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+    .item-button
+      line-height .44rem
+      margin-top .16rem
+      background #ff9300
+      padding 0 .2rem
+      border-radius .06rem
+      color #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" alt="" class="item-img">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeWeekend",
+    data () {
+      return {
+        recommendList: [{
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/source/1810/22/b036f54d85ea22.jpg_r_640x214_3e6eb1bb.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, {
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/source/1810/22/b036f54d85ea22.jpg_r_640x214_3e6eb1bb.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, {
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/source/1810/22/b036f54d85ea22.jpg_r_640x214_3e6eb1bb.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, {
+          id: "001",
+          imgUrl: "https://imgs.qunarzz.com/sight/source/1810/22/b036f54d85ea22.jpg_r_640x214_3e6eb1bb.jpg",
+          title: "秦皇岛野生动物园",
+          desc: "这个季节去温度还是挺舒适的"
+        }, ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl'
+.recommend-title
+  margin-top .2rem
+  background #eeeeee
+  line-height .8rem
+  text-indent .2rem
+.item-img-wrapper
+  overflow hidden
+  height 0
+  padding-bottom 33.9%
+  .item-img
+    width 100%
+.item-info
+  padding .1rem
+  .item-title
+    line-height .54rem
+    font-size .32rem
+    ellipsis()
+  .item-desc
+    line-height .4rem
+    color #ccc
+    ellipsis()
+</style>
